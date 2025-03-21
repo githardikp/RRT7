1f061fcdb19fa4a28bd02b90b1c1430fb76670cd
Hardik <hardikp1351@gmail.com>
2025-03-21 18:42:57 +0530

    added drawer navigation

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = ()=>{
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen;
