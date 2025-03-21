85caa0ac98b446f8e8a41632ded2a45cfa2efb5f
Hardik <hardikp1351@gmail.com>
2025-03-21 23:33:06 +0530

    added animation to keyboard and input text

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,48 +1,50 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView, TextInput } from "react-native";
-// import { TextInput } from "react-native-gesture-handler";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import Animated, { useAnimatedKeyboard, useAnimatedStyle } from "react-native-reanimated";
-import { SafeAreaView } from "react-native-safe-area-context";
-
-
-
-
 
 const HomeScreen = ()=>{
+    const keyboard = useAnimatedKeyboard();
+    const translateStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateY: -300}],
+        };
+    })
     return(
-        <View style={styles.conatiner}>
-            <Text>Home Screen</Text>
-            <ScrollView style={styles.inputContainer}>
-                    <TextInput 
-                        placeholder='Enter the task' 
-                        style={styles.inputBox}
-                        autoFocus={true}
-                        keyboardType='default'
-                        placeholderTextColor='black'
-                    />
-            </ScrollView>
+        <View style={styles.container}>
+            {/* <Text>Home Screen</Text> */}
+            <Animated.View style={[styles.inputContainer, translateStyle]}>
+                <TextInput 
+                    placeholder='Enter the task' 
+                    style={styles.inputBox}
+                    autoFocus={true}
+                    keyboardType='default'
+                    placeholderTextColor='black'
+                    // selectionColor={'black'}
+                    cursorColor={'black'}
+                />
+            </Animated.View>
         </View>
     )
 }
 
-
 const styles = StyleSheet.create({
-    conatiner:{
-        padding: 40,
-        flex:1,
-        // backgroundColor: 'red',
+    container: {
+        padding:40, 
+        paddingTop: 60,
+        flex: 1,
     },
-    inputContainer:{
-        flex:1,
-        justifyContent:'center',
-        // alignItems:'center',
-        // backgroundColor: 'blue',
+    inputContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        
     },
-    inputBox:{
+    inputBox: {
         borderColor: 'black',
         borderWidth: 1,
         borderRadius: 50,
-        
+        padding: 10,
+        color: 'black',
     }
 });
+
 export default HomeScreen;
