1b801e94e8d1d0056f73d1b250b195606f01a9b1
Hardik <hardikp1351@gmail.com>
2025-03-22 21:04:05 +0530

    added async storage and added data.js

---
+++ src/dataset/data.js
@@ -0,0 +1,22 @@
+export const tasks=[
+    {
+        'id': 1,
+        'task': 'This is task 1',
+    },
+    {
+        'id': 2,
+        'task': 'This is task 2',
+    },
+    {
+        'id': 3,
+        'task': 'This is task 3',
+    },
+    {
+        'id': 4,
+        'task': 'This is task 4',
+    },
+    {
+        'id': 5,
+        'task': 'This is task 5',
+    }
+]
